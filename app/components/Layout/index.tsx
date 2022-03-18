60f08434fe2de5a987f8db84eb29ce083567f2a5
ymkim <ji9@me.com>
2022-03-18 11:24:03 +0900

    google tag manager

--- app/components/Layout/index.tsx
+++ app/components/Layout/index.tsx
@@ -41,7 +41,7 @@ export function SiteMeta() {
         crossOrigin='anonymous'
       ></script>
       {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
-      <script async src='https://www.googletagmanager.com/gtag/js?id=G-NTZDCTHDX5'></script>
+      {/* <script async src='https://www.googletagmanager.com/gtag/js?id=G-NTZDCTHDX5'></script>
       <script
         dangerouslySetInnerHTML={{
           __html: `
@@ -52,7 +52,18 @@ export function SiteMeta() {
         gtag('config', 'G-NTZDCTHDX5');
       `,
         }}
+      ></script> */}
+      {/* <!-- Google Tag Manager --> */}
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-M6T8P2R');`,
+        }}
       ></script>
+      {/* <!-- End Google Tag Manager --> */}
     </>
   )
 }
