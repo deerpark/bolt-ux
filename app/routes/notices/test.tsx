27bf8fb9bc7b67930dca00389c6d4cba5ea65b83
ymkim <ji9@me.com>
2022-03-24 00:11:09 +0900

    prose style

---
+++ app/routes/notices/test.tsx
@@ -0,0 +1,15 @@
+import { useLocation } from 'react-router-dom'
+import { useNav, usePrevRoute, Route } from '~/lib/config'
+import { Layout } from '~/components/Layout'
+
+export default function Test() {
+  const { pathname } = useLocation()
+  const { title, Icon, desc, sidebar }: Route = useNav(pathname)
+  const prevRoute = usePrevRoute(pathname)
+
+  return (
+    <Layout {...{ title, Icon, desc, sidebar, prevRoute }}>
+      <div className='bx-sections'>^^</div>
+    </Layout>
+  )
+}
