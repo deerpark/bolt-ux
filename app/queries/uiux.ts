298c05d9d9164a918d701cab0ca12bc71ddceb90
ymkim <ji9@me.com>
2022-03-27 23:34:42 +0900

    add category queries

---
+++ app/queries/uiux.ts
@@ -0,0 +1,17 @@
+import { responsiveImageFragment, coverImage, author } from '~/lib/fragments'
+
+const news = `
+  {
+    posts: allPosts(orderBy: _createdAt_DESC, filter: {category: {eq: "120193011"}}, first: 20) {
+      title
+      slug
+      excerpt
+      date
+      ${coverImage}
+      ${author}
+    }
+  }
+  ${responsiveImageFragment}
+`
+
+export default news
